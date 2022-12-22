8e958a54fd338fbaee864d3f5c878099c07d555f
skunwar123 <sumankunwar152@gmail.com>
2022-12-22 16:40:51 +0545

    agwaaain

--- app/src/main/java/com/example/baraz/activities/LoginActivity.java
+++ app/src/main/java/com/example/baraz/activities/LoginActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.baraz.R;
 import com.example.baraz.databinding.ActivityLoginBinding;
 
+
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
