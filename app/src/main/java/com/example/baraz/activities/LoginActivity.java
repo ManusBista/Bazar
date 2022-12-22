44cae8a38c588e8f76c3fc2a006cc20358bd2877
skunwar123 <sumankunwar152@gmail.com>
2022-12-22 16:28:17 +0545

    another

--- app/src/main/java/com/example/baraz/activities/LoginActivity.java
+++ app/src/main/java/com/example/baraz/activities/LoginActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.baraz.R;
+import com.example.baraz.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
