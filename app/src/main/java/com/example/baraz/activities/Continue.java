546d3f65517ed45819d3889a971dbe231c52a421
skunwar123 <sumankunwar152@gmail.com>
2023-01-02 11:19:11 +0545

    design

--- app/src/main/java/com/example/baraz/activities/Continue.java
+++ app/src/main/java/com/example/baraz/activities/Continue.java
@@ -3,6 +3,11 @@ package com.example.baraz.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
 
 import com.example.baraz.R;
@@ -18,6 +23,20 @@ public class Continue extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityContinueBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        // Create a new shape drawable
+        ShapeDrawable drawable = new ShapeDrawable();
+
+        float radius = 15;
+        drawable.setShape(new RoundRectShape(new float[]{radius, radius, radius, radius, radius, radius, radius, radius}, null, null));
+// Set the shape's stroke width and color
+        drawable.getPaint().setStrokeWidth(8);
+        drawable.getPaint().setColor(Color.BLACK);
+        drawable.getPaint().setStyle(Paint.Style.STROKE);
+
+
+
+// Set the button's background to the drawable
+        binding.signUpBtn.setBackground(drawable);
 
         binding.cuntBtn.setOnClickListener(view -> {
             Intent intent = new Intent(Continue.this, MainActivity.class);
